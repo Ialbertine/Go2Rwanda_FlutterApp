834c27790d327ae587e405020ff9d2fcfe7a2808
IBIRASA <d.ishimwe@alustudent.com>
2024-10-25 14:51:18 +0200

    profile

--- front_end/lib/pages/Profile.dart
+++ front_end/lib/pages/Profile.dart
@@ -1,14 +1,8 @@
-// ignore_for_file: use_super_parameters, unused_import
-import 'package:flutter/material.dart';
-import './auth/login.dart';
-import './auth/register.dart';
-import './pages/landingpage.dart';
-import './pages/Homepage.dart';
-import './pages/Profile.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -132,7 +126,7 @@ class ProfilePage extends StatelessWidget {
                       child: CircleAvatar(
                         radius: 50,
                         backgroundImage: AssetImage(
-                            'assets/profile.jpg'), // Ensure the path is correct
+                            'assets/legacy.jpg'), // Ensure the path is correct
                         backgroundColor: Colors.green,
                       ),
                     ),
@@ -191,34 +185,4 @@ class ProfilePage extends StatelessWidget {
       ),
     );
   }
-}
-
-@override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'Go2Rwanda',
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      primarySwatch: Colors.green,
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: const Color(0xFF2E7D32),
-          foregroundColor: Colors.white,
-        ),
-      ),
-      outlinedButtonTheme: OutlinedButtonThemeData(
-        style: OutlinedButton.styleFrom(
-          foregroundColor: Colors.black,
-          side: const BorderSide(color: Colors.grey),
-        ),
-      ),
-    ),
-    home: const LandingPage(),
-    routes: {
-      '/login': (context) => const LoginPage(),
-      '/register': (context) => const RegisterPage(),
-      '/homepage': (context) => const HomePage(),
-      '/profile': (context) => ProfilePage(),
-    },
-  );
 }
